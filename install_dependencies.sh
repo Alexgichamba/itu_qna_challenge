989128038d4743dc9c696d8ed9e5c42c2106b5a5
Alex Gichamba <51129656+Alexgichamba@users.noreply.github.com>
2024-08-01 11:07:57 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# # Check if conda is installed
-# if ! command -v conda &> /dev/null
-# then
-#     echo "Conda could not be found. Please install Anaconda or Miniconda and try again."
-#     exit
-# fi
+# Check if conda is installed
+if ! command -v conda &> /dev/null
+then
+    echo "Conda could not be found. Please install Anaconda or Miniconda and try again."
+    exit
+fi
 
-# echo "Installing the dependencies using pip..."
-# pip install -r requirements.txt
+echo "Installing the dependencies using pip..."
+pip install -r requirements.txt
 
 
-# echo "Installing faiss-gpu using conda..."
-# conda install -y -c pytorch -c nvidia -c conda-forge faiss-gpu=1.8.0
+echo "Installing faiss-gpu using conda..."
+conda install -y -c pytorch -c nvidia -c conda-forge faiss-gpu=1.8.0
 
 # download documents
 echo "Downloading the documents..."
